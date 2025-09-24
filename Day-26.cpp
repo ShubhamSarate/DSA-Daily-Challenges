898126a247d41cbeae1b74649342303ad85736b4
ShubhamSarate <shubhusarate143@gmail.com>
2025-09-24 15:45:37 +0530

    Day-26.cpp

---
+++ Day-26.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int MaxWater(vector<int> &vec){
+
+   int st = 0;
+   int end = vec.size()-1;
+   int maxi = 0;
+
+   while(st < end){
+
+      int height = min(vec[st],vec[end]);
+      int width = end - st;
+      int area = height * width;
+
+      maxi = max(maxi,area);
+
+      if(vec[st] < vec[end]){
+         st ++;
+      }
+      else{
+         end --;
+      }
+   }
+   return maxi;
+}
+   
+int main()
+{
+   vector<int> vec = {1,8,6,2,5,4,8,3,7};
+
+   cout<<"Maximum Water Contain : "<<MaxWater(vec);
+   
+   return 0;
+}
